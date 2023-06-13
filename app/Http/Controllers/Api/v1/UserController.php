147e96e2da416f067c6ced880cbd242dc6c9bc64
piouskenny <kehindeu13@gmail.com>
2023-06-13 09:24:03 +0100

    created signup request

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(User $user)
+    {
+        // Select all tasks with the userID
+
+    }
+
+    public function signUp() {
+        
+    }
+
+
 }
