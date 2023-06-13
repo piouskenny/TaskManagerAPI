1d501ada6042ff3289eb1af394937260eb99377f
piouskenny <kehindeu13@gmail.com>
2023-06-13 10:18:35 +0100

    added login route to the API.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::get('/', [UserController::class, 'index']);
     Route::post('/signup', [UserController::class, 'signup']);
+    Route::post('/login', [UserController::class, 'login']);
 });
