6a8d63a07d1dacc02dbc3bf7082672d4d8e87bd4
piouskenny <kehindeu13@gmail.com>
2023-06-13 09:26:49 +0100

    made signuprequest rule

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -11,7 +11,7 @@ class SignupRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class SignupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'username' => 'string|required',
+            'email' => 'email|unique:users',
+            'password' => 'string|min:8'
         ];
     }
 }
