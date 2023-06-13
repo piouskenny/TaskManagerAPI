e0e332985a90d2f03c3231e0c901a13177e6fe20
piouskenny <kehindeu13@gmail.com>
2023-06-13 15:24:08 +0100

    cleaned up TaskController

--- app/Http/Controllers/Api/v1/TaskController.php
+++ app/Http/Controllers/Api/v1/TaskController.php
@@ -7,21 +7,6 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
